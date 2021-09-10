06573ece1841e6659ebbd90f81519e82e52de77b
guna7222 <rrgunasekhar@gmail.com>
2021-09-10 10:12:23 +0530

    dummy file

---
+++ file2.sh
@@ -0,0 +1,1 @@
+echo"Git (/ɡɪt/) is software for tracking changes in any set of files, usually used for coordinating work among programmers collaboratively developing source code during software development"

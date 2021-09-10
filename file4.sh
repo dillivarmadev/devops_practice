c04435fa997dad20c2ddc8daa83af2786d3fea18
guna7222 <rrgunasekhar@gmail.com>
2021-09-10 10:37:34 +0530

    shell book

---
+++ file4.sh
@@ -0,0 +1,6 @@
+echo"A shell is a special user program that provides an interface to the user to use operating system services. Shell accepts human-readable commands from the user and converts them into something which the kernel can understand. It is a command language interpreter that executes commands read from input devices such as keyboards or from files. The shell gets started when the user logs in or starts the terminal. 
+
+1). Displaying the file contents on the terminal: 
+
+cat: It is generally used to concatenate the files. It gives the output on the standard output.
+more: It is a filter for paging through text one screenful at a time."
